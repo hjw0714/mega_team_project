3fe15d4fc1db77415c27b16f37aefad1fbd6d6de
hjw0714 <yocasu1@gmail.com>
2025-02-11 11:05:34 +0900

    1

--- food_hub_project_test/src/main/java/com/application/foodhub/config/SecurityConfig.java
+++ food_hub_project_test/src/main/java/com/application/foodhub/config/SecurityConfig.java
@@ -22,5 +22,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
+    
+    
 }
 
