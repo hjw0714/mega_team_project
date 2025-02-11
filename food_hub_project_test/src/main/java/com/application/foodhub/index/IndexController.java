27762246e181cae8cd13d0c9c74d75e7742ac6b3
hjw0714 <yocasu1@gmail.com>
2025-02-11 11:17:45 +0900

    1

--- food_hub_project_test/src/main/java/com/application/foodhub/index/IndexController.java
+++ food_hub_project_test/src/main/java/com/application/foodhub/index/IndexController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	  @GetMapping
-	   public String index() {
-	      return "redirect:/foodhub" ;
-	   }
+
 	   
 	   @GetMapping("/foodhub")
 	   public String foodhub() {
