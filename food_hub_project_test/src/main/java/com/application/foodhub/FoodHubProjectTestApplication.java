959f1f78ffed37844b013b17ae0d658d1b993ed2
hjw0714 <yocasu1@gmail.com>
2025-04-30 15:33:36 +0900

    realfinal

--- food_hub_project_test/src/main/java/com/application/foodhub/FoodHubProjectTestApplication.java
+++ food_hub_project_test/src/main/java/com/application/foodhub/FoodHubProjectTestApplication.java
@@ -1,7 +1,11 @@
 package com.application.foodhub;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class FoodHubProjectTestApplication {
@@ -9,5 +13,10 @@ public class FoodHubProjectTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FoodHubProjectTestApplication.class, args);
 	}
+	
+	@PostConstruct
+    public void setTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 }
